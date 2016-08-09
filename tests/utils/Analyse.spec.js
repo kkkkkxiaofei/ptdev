69d19909d79712250bc940a3174d8009a169ee17
Xiaofei <xifzhang@thoughtworks.com>
2016-08-09 13:25:20 +0800

    add test for filtering stories by label

--- tests/utils/Analyse.spec.js
+++ tests/utils/Analyse.spec.js
@@ -2,16 +2,26 @@ import expect from 'expect'
 import * as Analyse from '../../src/utils/Analyse'
 
 describe('Analyse', () => {
-	it('filter stories by type', () => {
-		const stories = [
-			{story_type: 'bug', name: 'story1'},
-			{story_type: 'bug', name: 'story2'},
-			{story_type: 'feature', name: 'story3'}
+	let stories = []
+	beforeEach(() => {
+		stories = [
+			{story_type: 'bug', labels: [{name: 'a'}, {name: 'b'}], name: 'story1'},
+			{story_type: 'bug', labels: [{name: 'a'}, {name: 'c'}], name: 'story2'},
+			{story_type: 'feature', labels: [{name: 'c'}, {name: 'd'}], name: 'story3'}
 		]
+	})
 
+	it('filter stories by type', () => {
 		const filteredStories = Analyse.filterStoriesByType(stories, 'bug')
 
 		expect(filteredStories[0].name).toBe('story1')
 		expect(filteredStories[1].name).toBe('story2')
+	})
+
+	it('filter stories by label', () => {
+		const filteredStories = Analyse.filterStoriesByLabel(stories, 'c')
+
+		expect(filteredStories[0].name).toBe('story2')
+		expect(filteredStories[1].name).toBe('story3')
 	})
 })
