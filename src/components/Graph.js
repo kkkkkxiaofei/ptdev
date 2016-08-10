6739a07ea8e4b6b4febe7d442f4b498031a562bb
Xiaofei <xifzhang@thoughtworks.com>
2016-08-10 22:55:34 +0800

    add graph component

---
+++ src/components/Graph.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classNames from 'classnames'
+import { BarChart } from 'react-d3'
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (<div></div>) 
+  }
+}
+
+export default Graph
