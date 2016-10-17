3fa35de7222cc29a27d3a2e1bc7444d2bcc6e122
Xiaofei <xifzhang@thoughtworks.com>
2016-10-17 21:22:33 +0800

    move script from src to bin

--- bin/md2html.js
+++ bin/md2html.js
@@ -1,22 +1,22 @@
 var showdown  = require('showdown'),
 	fs = require('fs'),
-  converter = new showdown.Converter();
+  converter = new showdown.Converter(),
+  mdDir = '../src/md/',
+  viewsDir = '../src/md/views/';
 
-var dir = '../md';
-var viewsDir = '../md/views/';
-var files = fs.readdirSync(dir);
+var files = fs.readdirSync(mdDir);
 
 for(var i in files) {
 	var fileName = files[i];
-	var path = dir + '/' + fileName;
+	var path = mdDir + fileName;
 	var stat = fs.statSync(path);
 	if(stat.isFile()) {
 		var mdContent = fs.readFileSync(path).toString();
 		var html = converter.makeHtml(mdContent);
 		var wrapper = fs.readFileSync(viewsDir + 'wrapper.html').toString();
-		var result = wrapper.replace('{$content}', html).replace('{$title}', fileName);
-		console.log(result);
-		fs.writeFile(viewsDir + fileName + '.html', result, function(err) {
+		var newFileName = fileName.replace('.md', '');
+		var result = wrapper.replace('{$content}', html).replace('{$title}', newFileName);
+		fs.writeFile(viewsDir + newFileName + '.html', result, function(err) {
 			if(err) return console.log(err)
 		});
 	}
