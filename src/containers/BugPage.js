2c87a482a041e3773d4d4e96306daa29c7a11961
Xiaofei <xifzhang@thoughtworks.com>
2016-10-19 22:52:21 +0800

    [xf] render bugpage

--- src/containers/BugPage.js
+++ src/containers/BugPage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Graph from '../components/Graph'
 import StoryList from '../components/StoryList'
+import Search from '../components/Search'
 import * as Analyse from '../utils/Analyse'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 export default class BugPage extends React.Component {
   constructor(props) {
@@ -9,10 +11,17 @@ export default class BugPage extends React.Component {
     this.bugTendencyAnalyse = this.bugTendencyAnalyse.bind(this)
     this.bugSeverityAnalyse = this.bugSeverityAnalyse.bind(this)
     this.bugCategoryAnalyse = this.bugCategoryAnalyse.bind(this)
+    this.subRender = this.subRender.bind(this)
+    this.state = {stories: []}
+    this.setStories = this.setStories.bind(this)
+  }
+
+  setStories(stories) {
+    this.setState({stories: stories})
   }
 
 	bugTendencyAnalyse() {
-    const stories = this.props.stories
+    const stories = this.state.stories
     const graphData = Analyse.generateTendencyByType(stories, 'bug')
     return {
       graphData: graphData,
@@ -21,7 +30,7 @@ export default class BugPage extends React.Component {
   }
 
   bugSeverityAnalyse() {
-    const stories = this.props.stories
+    const stories = this.state.stories
     const graphData = Analyse.generateSeverity(stories)
     return {
       graphData: graphData,
@@ -30,7 +39,7 @@ export default class BugPage extends React.Component {
   }
 
   bugCategoryAnalyse() {
-    const stories = this.props.stories
+    const stories = this.state.stories
     const graphData = Analyse.generateBugCategory(stories, 'bug')
     
     return {
@@ -39,23 +48,37 @@ export default class BugPage extends React.Component {
     }
   }
 
+  subRender() {
+    const stories = this.state.stories
+    if(stories.length > 0) {
+      return (
+        <div>
+          <div className="graphList">
+            <div className="graphListContainer">
+              {[this.bugTendencyAnalyse, this.bugSeverityAnalyse, this.bugCategoryAnalyse].map(method => {
+                return (
+                  <Graph {...method()} />
+                )
+              })}
+            </div>
+          </div>
+          <div className="storyList" >
+            <StoryList stories={stories} storyType="bug" />
+          </div>
+        </div>
+      )
+    }
+  }
+
   render() {
-  	const {stories} = this.props
+  	const stories = this.state.stories
     return (
-      <div className="storyPage">
-      	<div className="graphList">
-      		<div className="graphListContainer">
-		  			{[this.bugTendencyAnalyse, this.bugSeverityAnalyse, this.bugCategoryAnalyse].map(method => {
-		  				return (
-		  					<Graph {...method()} />
-		  				)
-		  			})}
-      		</div>
-	  		</div>
-	  		<div className="storyList" >
-			  	{stories.length ? (<StoryList stories={stories} storyType="bug" />) : ''}
-	  		</div>
-      </div>
+      <MuiThemeProvider>
+        <div className="storyPage">
+          <Search action={{setStories: this.setStories}} />
+          {this.subRender()}	
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
