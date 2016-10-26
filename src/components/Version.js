af4f361292d48be3d54769e88aa1721b1f99baa1
Xiaofei <xifzhang@thoughtworks.com>
2016-10-26 10:36:43 +0800

    [xf] #8 store version into localstorage

--- src/components/Version.js
+++ src/components/Version.js
@@ -20,11 +20,16 @@ export default class Version extends React.Component {
 	}
 
 	compareVersion(res) {
-		const VERSION = `reference: misk5_15.25.47.14
-										commit: 1d8451d07ecc792b69b4b5b8e2586f1e5108420d
-										integrated_tag: int_prd_15.38.61.14
-										deployed: Mon Oct 24 13:03:47 2016 -0500`
-		this.setState({isEqual: res == VERSION})
+		let VERSION = localStorage["VERSION"]
+		if(VERSION) {
+			let versionOject = JSON.parse(VERSION)
+			this.setState({isEqual: versionOject.content == res})
+		} else {
+			localStorage["VERSION"] = JSON.stringify({
+				date: new Date(),
+				content: res
+			})
+		}
 	}
 
 	render() {
