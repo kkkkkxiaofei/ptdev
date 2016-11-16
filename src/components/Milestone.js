ddfc87ce13e2b048abeac05fb23e476110c5ce37
Xiaofei <xifzhang@thoughtworks.com>
2016-11-16 10:06:45 +0800

    [xf] change milestone

--- src/components/Milestone.js
+++ src/components/Milestone.js
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 import Slider from 'material-ui/Slider';
 
 const BEGINING = new Date("Thu Oct 13 2016 00:00:00 GMT+0800 (CST)")
-const ENDING = new Date("Thu Dec 15 2016 00:00:00 GMT+0800 (CST)")
+const ENDING = new Date("Thu Dec 20 2016 00:00:00 GMT+0800 (CST)")
 const DAY = 1000*60*60*24
 const TOTAL = (ENDING - BEGINING)/DAY
 
@@ -27,7 +27,7 @@ export default class Milestone extends React.Component {
 		return (
 		  <div className="step">
 		    <Slider defaultValue={this.state.defaultValue} />
-		  	<div className="ending bgGreen">Final Release(Dec 15)</div>
+		  	<div className="ending bgGreen">Final Release(Dec 20)</div>
 		  </div>
 		)
 	}
